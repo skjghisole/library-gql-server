0818e92ce509eaff7866ff549efd327bb57b6800
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-04 01:31:20 -0400

    separated Server from Client

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,8 +2,12 @@ import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from '../schema'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
+
 
 mongoose.connect('mongodb://admin:test123@ds229078.mlab.com:29078/library')
 mongoose.connection.once('open', () => {
